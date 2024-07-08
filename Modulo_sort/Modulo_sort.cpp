ebb6a20a80f0091036569530e6cbfd6ad47d86b7
Andrei_Gridin <andreigridin918@gmail.com>
2024-07-08 08:54:04 +0500

    Добавил комментарии

--- Modulo_sort/Modulo_sort.cpp
+++ Modulo_sort/Modulo_sort.cpp
@@ -1,10 +1,9 @@
 ﻿#include <iostream>
 #include <cstdlib>
-#include <cmath>
 
 using namespace std;
 
-void method_sort(int* array, size_t n)
+void method_sort(int* array, size_t n)                    // Метод сортирует числа по модулю
 {
     for (int i = 0; i < n / 2; i++)
     {
@@ -18,7 +17,7 @@ void method_sort(int* array, size_t n)
     }
 }
 
-void show_array(int* array, size_t n)
+void show_array(int* array, size_t n)                     // Метод выводит отсортированный массив в терминал
 {
     cout << " Вывод: ";
     for (int i = 0; i < n; i++)
@@ -46,10 +45,10 @@ int main()
     system("chcp 1251>nul");
     system("color 80");
 
-    int arr[6]{ -100,-50,-5,1,10,15 };
+    int arr[]{ -100,-50,-5,1,10,15 };                     // Объявляем и инициализируем массив
     
-    method_sort(arr, size(arr));
-    show_array(arr, size(arr));
+    method_sort(arr, size(arr));                          // Вызываем метод сортировки с передачей ему массива и размера массива
+    show_array(arr, size(arr));                           // Вызываем метод вывода в терминал с передачей ему массива и размера массива
 
     system("pause>nul");
     return 0;
